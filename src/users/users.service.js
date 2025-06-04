7e596ae28181573501ccd6afa07f9f34a4087e66
jlopes <joao.victor@lojainterativa.com>
2025-06-03 21:19:01 -0300

    ✨ Adiciona dependência `moment` e validação de data de nascimento no cadastro de usuários

--- src/users/users.service.js
+++ src/users/users.service.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs');
 const Usuario = require('./users.model');
+const moment = require('moment');
 const {generateToken} = require('../auth/jwt'); // Importa a função para gerar o token
 
 const criarUsuario = async (dadosUsuario) => {
@@ -9,6 +10,15 @@ const criarUsuario = async (dadosUsuario) => {
         if (emailExistente) {
             throw new Error('E-mail já cadastrado');
         }
+
+        // Verifica e formata a data de nascimento
+        const dataFormatada = moment(dadosUsuario.data_nascimento, 'DD/MM/YYYY', true);
+        if (!dataFormatada.isValid()) {
+            throw new Error('Data de nascimento inválida. Use o formato DD/MM/YYYY.');
+        }
+
+        // Converte para o formato ISO (YYYY-MM-DD)
+        dadosUsuario.data_nascimento = dataFormatada.format('YYYY-MM-DD');
 
         // Hash da senha
         const senhaHash = await bcrypt.hash(dadosUsuario.senha, 10);
